21cfc4657ef101dce72e999d15d09ddbed3d3b94
andrian1r <andrianwijaya2701@gmail.com>
2026-02-14 15:31:06 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+#ini index string
+
+huruf = "Hello"
+print(huruf[-1])
+
+#ini index angka
+angka = [1 , 2 , 3 , 4 , 5]
+print (angka[-1])
+
+#ini tipe data string
+panjang = "besi"
+print (panjang)
+print (type(panjang))
+
+#ini tipe data interger
+umur = 100
+print(umur)
+print (type(umur))
+
+#ini adalah undescroe untuk memudahkan pengguna(_undescroe gabakal dicetak pas di run
+print(111_111_111)
+
+#float
+nilai_pts_mtk = 8.5
+print(nilai_pts_mtk)
+print(type(nilai_pts_mtk))
+
+#boolean
+berhasil = True
+gagal = False
+print(berhasil , gagal)
+print( type(berhasil), type(gagal) )
+
+#string + int = error
+# lebar = 10
+# andrian = ("pelajar")
+# print (lebar + andrian)
+
+nama = ("andrian", "wijaya ", "santoso")
+umur = 13
+tinggi = 1.50
+hobby = ("maen game", "basket")
+print("====Biodata andrian====")
+print("nama:",nama[0],nama[2],nama[1])
+print("umur:",umur,"tahun")
+print("Tinggi", tinggi, "m")
+print("hobi:", hobby[0])
+
+
+
